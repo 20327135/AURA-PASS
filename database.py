c18076ca04d4778f9458440af01aa76b2a23647a
begoodandshine365-beep <begoodandshine365@gmail.com>
2026-08-21 05:33:43 +0900

    fix: DB 연결 완벽 해결

--- database.py
+++ database.py
@@ -1,15 +1,14 @@
-
-import os
-from dotenv import load_dotenv
 from motor.motor_asyncio import AsyncIOMotorClient
 
-load_dotenv()
 
-MONGO_DETAILS = os.getenv("MONGO_DETAILS")
+MONGO_DETAILS = "mongodb+srv://begoodandshine365_db_user:pWmG0CubsMvOCdKJ@cluster0.bcgk3v7.mongodb.net/?retryWrites=true&w=majority&appName=aura-pass"
+
 
 client = AsyncIOMotorClient(
     MONGO_DETAILS,
     tls=True,
     tlsAllowInvalidCertificates=True
 )
+
+# 사용할 데이터베이스 이름
 database = client.aura_pass
